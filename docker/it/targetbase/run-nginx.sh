34567bf29a618ca3fc8e8994862df729026068fa
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-06-11 13:59:05 +0200

    FIX: The tls130rtt target should only fail the 0-RTT test. Configure NGINX correctly for SSL stapling and HSTS to prevent other tests failing.

--- docker/it/targetbase/run-nginx.sh
+++ docker/it/targetbase/run-nginx.sh
@@ -10,6 +10,12 @@ perl -pi -e 's|error_log /var/log/nginx/error.log|error_log /var/log/nginx/error
 
 rm /etc/nginx/sites-enabled/default
 mv $1 /etc/nginx/sites-enabled/
+
+# Work around error: "ssl_stapling" ignored, host not found in OCSP responder "ca-ocsp.test.nlnetlabs.nl:8080" in the certificate "/etc/ssl/certs/xxx.crt"
+while ! host ca-ocsp.test.nlnetlabs.nl; do
+    sleep 1s
+done
+
 service nginx restart
 
 tail -n 1000 -F /var/log/nginx/*.log
