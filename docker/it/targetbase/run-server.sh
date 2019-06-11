81498f9ec68e43e741507b316e0f6fc03dd668c4
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-06-11 04:53:35 +0200

    FIX: Make ssl2 and ssl3 containers start up correctly.

--- docker/it/targetbase/run-server.sh
+++ docker/it/targetbase/run-server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-set -e -u
+set -e -u -x
 
 service apache2 start
 
-/usr/bin/tail -F /var/log/apache2/*.log
+/usr/bin/tail -F /var/log/apache2/*.log /var/log/custom-server.log &
+
+if [ $# -gt 0 ]; then
+    $* &>/var/log/custom-server.log
+fi
+
+sleep infinity
