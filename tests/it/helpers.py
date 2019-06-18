09d42f2dc2fb8d642f64cfdb0d414167e40ef85e
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-06-18 12:55:16 +0200

    E2E/Integration test suite coverage and capability improvements.

--- tests/it/helpers.py
+++ tests/it/helpers.py
@@ -1,3 +1,5 @@
+# TODO: Use Selenium Page Objects
+import re
 import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
@@ -8,13 +10,18 @@ from selenium.webdriver.support import expected_conditions as EC
 BASE_URL = 'http://internet.nl:8080/'
 
 LOCATOR_REPORT_SHOW_DETAILS_BUTTON_CLASS = 'panel-button-show'
+LOCATOR_PASSED_TEST_CLASS = 'testresult passed'
+LOCATOR_WARNING_TEST_CLASS = 'testresult warning'
 LOCATOR_FAILED_TEST_CLASS = 'testresult failed'
+LOCATOR_NOTTESTED_TEST_CLASS = 'testresult not-tested'
+
 LOCATOR_PROBING_CLASS = 'probing'
 LOCATOR_PROBE_CATEGORY_SUFFIX = '-summary'
 LOCATOR_PROBE_RUNNING_TEXT = 'Running...'
 LOCATOR_WEBSITE_TEST_FORM_ID = 'web-url'
 LOCATOR_RESULTS_OVERVIEW_ID = 'testresults-overview'
 LOCATOR_PROGRESS_AND_RESULT_TITLE_PREFIX = 'Website test: '
+LOCATOR_SCORE = 'score'
 
 XPATH_PROBES = (
     '//*[@class=\'{}\']'
@@ -24,34 +31,54 @@ XPATH_PROBES = (
     LOCATOR_PROBING_CLASS,
     LOCATOR_PROBE_CATEGORY_SUFFIX,
     LOCATOR_PROBE_RUNNING_TEXT)
-
-XPATH_FAILED_TESTS = (
-    '//*[@class=\'{}\']/h3/a'
-    ).format(LOCATOR_FAILED_TEST_CLASS)
+XPATH_TEST_TITLE = '//div[@class=\'{}\']/h3/a'
+XPATH_TEST_DETAILS_TABLE_BODY_ROWS = (
+    '//div['
+    'contains(@class,\'testresult\') and '
+    'h3/a[text()[contains(.,\'{}\')]]'
+    ']'
+    '//div[@class=\'tech-details-table\']/table/tbody/tr'
+)
 
 
 class TESTS:
-    CERT_TRUST = 'Trust chain of certificate'
-    DOMAIN_NAME_ON_CERT = 'Domain name on certificate'
-    IPV6_ADDRESS_FOR_WEB_SERVER = 'IPv6 addresses for web server'
-    HSTS = 'HSTS'
-    HTTPS_AVAILABLE = 'HTTPS available'
-    SAME_WEBSITE_ON_IPV4_AND_IPV6 = 'Same website on IPv6 and IPv4'
-    TLS_VERSION = 'TLS version'
-    ZERO_RTT = '0-RTT'
-    OCSP_STAPLING = 'OCSP Stapling'
-    CIPHER_SUITES = 'Cipher suites'
-    CLIENT_RENEG = 'Client-initiated renegotiation'
+    DANE_EXISTS = 'DANE existence'
+    DANE_VALID = 'DANE validity'
+    DNSSEC_EXIST = 'DNSSEC existence'
+    DNSSEC_VALID = 'DNSSEC validity'
+    HTTPS_CERT_DOMAIN = 'Domain name on certificate'
+    HTTPS_CERT_PUBKEY = 'Public key of certificate'
+    HTTPS_CERT_SIG = 'Signature of certificate'
+    HTTPS_CERT_TRUST = 'Trust chain of certificate'
+    HTTPS_HTTP_COMPRESSION = 'HTTP compression'
+    HTTPS_HTTP_HSTS = 'HSTS'
+    HTTPS_HTTP_HTTPS_AVAILABLE = 'HTTPS available'
+    HTTPS_HTTP_REDIRECT = 'HTTPS redirect'
+    HTTPS_TLS_CIPHER_SUITES = 'Cipher suites'
+    HTTPS_TLS_CLIENT_RENEG = 'Client-initiated renegotiation'
+    HTTPS_TLS_COMPRESSION = 'TLS compression'
+    HTTPS_TLS_KEY_EXCHANGE = 'Key exchange parameters'
+    HTTPS_TLS_OCSP_STAPLING = 'OCSP Stapling'
+    HTTPS_TLS_SECURE_RENEG = 'Secure renegotiation'
+    HTTPS_TLS_VERSION = 'TLS version'
+    HTTPS_TLS_ZERO_RTT = '0-RTT'
+    IPV6_NS_ADDRESS = 'IPv6 addresses for name servers'
+    IPV6_NS_REACHABILITY = 'IPv6 reachability of name servers'
+    IPV6_WEB_ADDRESS = 'IPv6 addresses for web server'
+    IPV6_WEB_REACHABILITY = 'IPv6 reachability of web server'
+    IPV6_WEB_SAME_WEBSITE = 'Same website on IPv6 and IPv4'
+    SECURITY_HTTP_XFRAME = 'X-Frame-Options'
+    SECURITY_HTTP_XCONTYPE = 'X-Content-Type-Options'
+    SECURITY_HTTP_XXSS = 'X-XSS-Protection'
+    SECURITY_HTTP_CSP = 'Content-Security-Policy existence'
+    SECURITY_HTTP_REFERRER = 'Referrer-Policy existence'
 
 
 class UX:
     @staticmethod
-    def get_failed_tests(selenium):
-        testresult_failed_anchors = selenium.find_elements(
-            By.XPATH, XPATH_FAILED_TESTS)
-
-        failed_tests = set()
-        for title_anchor_of_failed_test in testresult_failed_anchors:
+    def _get_test_names(testresult_anchors):
+        test_titles = set()
+        for title_anchor_of_failed_test in testresult_anchors:
             # This element should be an anchor ('<a>') child of a <div>
             # structure like so:
             # <div class='testresult_failed'>
@@ -71,13 +98,48 @@ class UX:
             # WebElement.text() contains it. The DOM elements don't always
             # appear to be in the same order so we can't just use the 2nd text
             # node as sometimes that isn't TEST TITLE but instead is 'open'.
-            internetnl_test_title = title_anchor_of_failed_test.text \
-                .replace("Failed:", '') \
-                .replace("open", '').strip() \
-                .replace("close", '').strip()
-            failed_tests.add(internetnl_test_title)
+            test_title = re.sub(r'(open|close|[A-Za-z ]+:)', '',
+                title_anchor_of_failed_test.text).strip()
+            test_titles.add(test_title)
 
-        return failed_tests
+        return test_titles
+
+    @staticmethod
+    def _get_matching_tests(selenium, test_class):
+        testresult_anchors = selenium.find_elements(
+            By.XPATH, XPATH_TEST_TITLE.format(test_class))
+        return UX._get_test_names(testresult_anchors)
+
+    @staticmethod
+    def get_failed_tests(selenium):
+        return UX._get_matching_tests(selenium, LOCATOR_FAILED_TEST_CLASS)
+
+    @staticmethod
+    def get_warning_tests(selenium):
+        return UX._get_matching_tests(selenium, LOCATOR_WARNING_TEST_CLASS)
+
+    @staticmethod
+    def get_nottested_tests(selenium):
+        return UX._get_matching_tests(selenium, LOCATOR_NOTTESTED_TEST_CLASS)
+
+    @staticmethod
+    def get_score(selenium):
+        return selenium.find_element(By.CLASS_NAME, LOCATOR_SCORE).text
+
+    @staticmethod
+    def get_test_detail_table_rows(selenium, test_title):
+        xpath = XPATH_TEST_DETAILS_TABLE_BODY_ROWS.format(test_title)
+        return selenium.find_elements_by_xpath(xpath)
+
+    @staticmethod
+    def get_table_values(selenium, test_title):
+        rows = []
+        for tr in UX.get_test_detail_table_rows(selenium, test_title):
+            cells = []
+            for td in tr.find_elements_by_tag_name('td')[1:]:
+                cells.append(td.text.strip())
+            rows.append(cells)
+        return rows
 
     @staticmethod
     def results_overview_is_present(selenium):
@@ -157,9 +219,24 @@ class UX:
 
 
 class DomainConfig:
-    def __init__(self, domain, expected_failures={}):
+    def __init__(self, domain, expected_failures={}, expected_warnings={},
+                 expected_not_tested={}, expected_score=None):
         self.domain = domain
         self.expected_failures = set(expected_failures)
+        self.expected_warnings = set(expected_warnings)
+        self.expected_not_tested = set(expected_not_tested)
+        self.expected_score = expected_score
+
+
+class GoodDomain(DomainConfig):
+    def __init__(self, domain, not_tested={}):
+        super().__init__(domain, expected_not_tested=not_tested,
+            expected_score='100%')
+
+
+class BadDomain(DomainConfig):
+    def __init__(self, domain, failures={}):
+        super().__init__(domain, expected_failures=failures)
 
 
 def id_generator(val):
--- tests/it/test_tls.py
+++ tests/it/test_tls.py
@@ -1,36 +1,110 @@
-# TODO: Use Selenium Page Objects
-# Don't test the success percentage yet because the target servers use
-# self-signed SSL certificates which do not pass the certificate trust
-# chain test and thus we cannot achieve a 100% score. Also, a score of
-# 100% doesn't say anything about how tests or which tests passed.
-
 import pytest
-from helpers import DomainConfig, id_generator, TESTS, UX
+from helpers import DomainConfig, GoodDomain, BadDomain
+from helpers import id_generator, TESTS, UX
 
 
 domains_configured_to_pass = [
-    DomainConfig('tls1213.test.nlnetlabs.nl'),
-    DomainConfig('tls1213sni.test.nlnetlabs.nl'),
-    DomainConfig('tls12only.test.nlnetlabs.nl'),
-    DomainConfig('tls13only.test.nlnetlabs.nl'),
+    # 0-RTT is a TLS 1.3 feature so should not be tested.
+    GoodDomain('tls12only.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_ZERO_RTT}),
+    GoodDomain('tls1213.test.nlnetlabs.nl'),
+    GoodDomain('tls1213sni.test.nlnetlabs.nl'),
+    GoodDomain('tls12onlyffdhe3072.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_ZERO_RTT}),
+    GoodDomain('tls13only.test.nlnetlabs.nl'),
 ]
-
 
 domains_configured_to_fail = [
     # no SSL means no HTTPS
     DomainConfig(
         'nossl.test.nlnetlabs.nl',
         expected_failures={
-            TESTS.HTTPS_AVAILABLE
+            TESTS.HTTPS_HTTP_HTTPS_AVAILABLE
+        },
+        expected_not_tested={
+            TESTS.DANE_EXISTS,
+            TESTS.DANE_VALID,
+            TESTS.HTTPS_CERT_DOMAIN,
+            TESTS.HTTPS_CERT_PUBKEY,
+            TESTS.HTTPS_CERT_SIG,
+            TESTS.HTTPS_CERT_TRUST,
+            TESTS.HTTPS_HTTP_COMPRESSION,
+            TESTS.HTTPS_HTTP_HSTS,
+            TESTS.HTTPS_HTTP_REDIRECT,
+            TESTS.HTTPS_TLS_CIPHER_SUITES,
+            TESTS.HTTPS_TLS_CLIENT_RENEG,
+            TESTS.HTTPS_TLS_COMPRESSION,
+            TESTS.HTTPS_TLS_KEY_EXCHANGE,
+            TESTS.HTTPS_TLS_OCSP_STAPLING,
+            TESTS.HTTPS_TLS_SECURE_RENEG,
+            TESTS.HTTPS_TLS_VERSION,
+            TESTS.HTTPS_TLS_ZERO_RTT,
+            TESTS.SECURITY_HTTP_CSP,
+            TESTS.SECURITY_HTTP_REFERRER,
+            TESTS.SECURITY_HTTP_XCONTYPE,
+            TESTS.SECURITY_HTTP_XFRAME,
+            TESTS.SECURITY_HTTP_XXSS,
         }),
 
+    # This domain deliberately lacks an IPV6 AAAA record in DNS
+    DomainConfig(
+        'tls1213ipv4only.test.nlnetlabs.nl',
+        expected_failures={
+            TESTS.IPV6_WEB_ADDRESS
+        },
+        expected_not_tested={
+            TESTS.IPV6_WEB_REACHABILITY,
+            TESTS.IPV6_WEB_SAME_WEBSITE
+        }),
+
+    # This domain deliberately has no matching virtual host configuration on
+    # the webserver that its DNS A and AAAA records resolve to.
+    DomainConfig(
+        'tls1213wrongcertname.test.nlnetlabs.nl',
+        expected_failures={
+            TESTS.HTTPS_CERT_DOMAIN,
+        },
+        expected_not_tested={
+            TESTS.DANE_VALID
+        }),
+
+    # This website virtual host configuration deliberately fails to serve a
+    # HSTS response header
+    BadDomain('tls1213nohsts.test.nlnetlabs.nl', {TESTS.HTTPS_HTTP_HSTS}),
+
+    # This website virtual host configuration deliberately serves a 'short'
+    # HSTS response header.
+    # TODO: Extend this test to detect that the report complains specifically
+    # about a 'short' HSTS.
+    BadDomain('tls1213shorthsts.test.nlnetlabs.nl', {TESTS.HTTPS_HTTP_HSTS}),
+
+    # This website virtual host configuration deliberately does not do OCSP
+    # stapling.
+    BadDomain('tls1213noocspstaple.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_OCSP_STAPLING}),
+
+    # This website virtual host configuration deliberately supports 0-RTT
+    # Note: if NGINX hasn't already fetched the OCSP responder response it will
+    # not staple the OCSP responder data in the response to us and this test
+    # will fail. NGINX should be configured to serve OCSP responder data from
+    # a file to avoid this issue.
+    BadDomain('tls130rtt.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_ZERO_RTT}),
+]
+
+domains_with_phase_out_warnings = [
     # Our TLS 1.0 server uses the OpenSSL server binary which does not serve
     # HSTS response headers
     # TODO: Extend this test to check that TLS 1.0 is flagged as phase out.
     DomainConfig(
         'tls10only.test.nlnetlabs.nl',
         expected_failures={
-            TESTS.HSTS
+            TESTS.HTTPS_HTTP_HSTS
+        },
+        expected_warnings={
+            TESTS.HTTPS_TLS_VERSION: [
+                ['TLS 1.0 (at risk)'],  # IPv6
+                ['TLS 1.0 (at risk)'],  # IPv4
+            ]
+        },
+        expected_not_tested={
+            TESTS.HTTPS_TLS_ZERO_RTT
         }),
 
     # Our TLS 1.1 server uses the OpenSSL server binary which does not serve
@@ -39,84 +113,48 @@ domains_configured_to_fail = [
     DomainConfig(
         'tls11only.test.nlnetlabs.nl',
         expected_failures={
-            TESTS.HSTS
+            TESTS.HTTPS_HTTP_HSTS
+        },
+        expected_warnings={
+            TESTS.HTTPS_TLS_VERSION: [
+                ['TLS 1.1 (at risk)'],  # IPv6
+                ['TLS 1.1 (at risk)'],  # IPv4
+            ]
+        },
+        expected_not_tested={
+            TESTS.HTTPS_TLS_ZERO_RTT
         }),
 
-    # This domain deliberately lacks an IPV6 AAAA record in DNS
+    # 0-RTT is a TLS 1.3 feature so should not be tested.
+    # Finite-field group ffdhe2048 is listed as 'phase out' by NCSC 2.0 and
+    # so should result in a perfect score and a warning about the ff group.
     DomainConfig(
-        'tls1213ipv4only.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.IPV6_ADDRESS_FOR_WEB_SERVER
-        }),
-
-    # This domain deliberately has no matching virtual host configuration on
-    # the webserver that its DNS A and AAAA records resolve to.
-    DomainConfig(
-        'tls1213wrongcertname.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.DOMAIN_NAME_ON_CERT
-        }),
-
-    # This website virtual host configuration deliberately fails to serve a
-    # HSTS response header
-    DomainConfig(
-        'tls1213nohsts.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.HSTS
-        }),
-
-    # This website virtual host configuration deliberately serves a 'short'
-    # HSTS response header.
-    # TODO: Extend this test to detect that the report complains specifically
-    # about a 'short' HSTS.
-    DomainConfig(
-        'tls1213shorthsts.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.HSTS
-        }),
-
-    # This website virtual host configuration deliberately does not do OCSP
-    # stapling.
-    DomainConfig(
-        'tls1213noocspstaple.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.OCSP_STAPLING
-        }),
-
-    # This website virtual host configuration deliberately supports 0-RTT
-    # Note: if NGINX hasn't already fetched the OCSP responder response it will
-    # not staple the OCSP responder data in the response to us and this test
-    # will fail. NGINX should be configured to serve OCSP responder data from
-    # a file to avoid this issue.
-    DomainConfig(
-        'tls130rtt.test.nlnetlabs.nl',
-        expected_failures={
-            TESTS.ZERO_RTT
-        }),
+        'tls12onlyffdhe2048.test.nlnetlabs.nl',
+        expected_warnings={
+            TESTS.HTTPS_TLS_KEY_EXCHANGE: [
+                ['DH-FFDHE2048 (at risk)'],  # IPv6
+                ['DH-FFDHE2048 (at risk)'],  # IPv4
+            ]
+        },
+        expected_not_tested={
+            TESTS.HTTPS_TLS_ZERO_RTT
+        },
+        expected_score='100%'),
 ]
-
 
 mishandled_domains = [
     # internet.nl cannot make SSLv2 connections so instead of failing because
     # of the insecure TLS version it fails because it cannot detect HTTPS
     # support at al.
     pytest.param(
-        DomainConfig(
-            'ssl2only.test.nlnetlabs.nl',
-            expected_failures={
-                TESTS.TLS_VERSION
-            }),
+        BadDomain('ssl2only.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_VERSION}),
         marks=pytest.mark.xfail),
 
     # internet.nl cannot make SSLv3 connections so instead of failing because
     # of the insecure TLS version it fails because it cannot detect HTTPS
     # support at al.
     pytest.param(
-        DomainConfig(
-            'ssl3only.test.nlnetlabs.nl',
-            expected_failures={
-                TESTS.TLS_VERSION
-            }),
+        BadDomain('ssl3only.test.nlnetlabs.nl', {TESTS.HTTPS_TLS_VERSION}),
         marks=pytest.mark.xfail),
 ]
 
@@ -126,12 +164,30 @@ def assess_website(selenium, domain_config):
     UX.wait_for_test_to_start(selenium, domain_config.domain)
     UX.wait_for_test_to_complete(selenium)
     UX.open_report_detail_sections(selenium)
+
     assert UX.get_failed_tests(selenium) == domain_config.expected_failures
+    assert UX.get_nottested_tests(selenium) == domain_config.expected_not_tested
+
+    if domain_config.expected_warnings is set:
+        assert UX.get_warning_tests(selenium) == domain_config.expected_warnings
+    elif domain_config.expected_warnings is dict:
+        assert UX.get_warning_tests(selenium) == set(domain_config.expected_warnings.keys())
+        for test_title, details in domain_config.expected_warnings:
+            assert UX.get_table_values(selenium, test_title) == details
+
+    if domain_config.expected_score:
+        assert UX.get_score(selenium) == domain_config.expected_score
 
 
 @pytest.mark.parametrize(
     'domain_config', domains_configured_to_pass, ids=id_generator)
 def test_domains_configured_to_pass(selenium, domain_config):
+    assess_website(selenium, domain_config)
+
+
+@pytest.mark.parametrize(
+    'domain_config', domains_with_phase_out_warnings, ids=id_generator)
+def test_domains_with_phaseout_warnings(selenium, domain_config):
     assess_website(selenium, domain_config)
 
 
@@ -145,3 +201,7 @@ def test_domains_configured_to_fail(selenium, domain_config):
     'domain_config', mishandled_domains, ids=id_generator)
 def test_domains_mishandled_by_internetnl(selenium, domain_config):
     assess_website(selenium, domain_config)
+
+
+# What do we want?
+# - phased out configurations should be 100% score but warning status
