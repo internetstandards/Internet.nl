b5ce7cc154b6559999d517c548b5be3d0738d79f
Maarten Aertsen <maarten.aertsen@ncsc.nl>
2021-10-18 16:51:17 +0200

    celery: use 'CELERY_' prefix consistently per Celery docs
    Made using `celery upgrade settings internetnl/settings.py-dist --django`
    
    Signed-off-by: Maarten Aertsen <maarten.aertsen@ncsc.nl>

--- internetnl/celery.py
+++ internetnl/celery.py
@@ -9,7 +9,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internetnl.settings')
 
 app = Celery('internetnl')
 
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
