internetstandards/Internet.nl

commit 4407156ba9391cd437e44095dba938f7c719f233

periodic_task is deprecated, use new beat_schedule configuration

checks/batch/scheduler.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import random
 from timeit import default_timer as timer
 
-from celery.decorators import periodic_task
+from internetnl.celery import app
 from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.core.cache import cache
@@ -583,8 +583,7 @@ def _run_scheduler():
 
 
 if settings.ENABLE_BATCH:
-    @periodic_task(run_every=timedelta(
-        seconds=settings.BATCH_SCHEDULER_INTERVAL))
+    @app.task(name='run_batch')
     def run():
         """
         Run the scheduler every interval only if it is not running already.

checks/tasks/update.py
@@ -1,7 +1,5 @@
 # Copyright: 2019, NLnet Labs and the Internet.nl contributors
 # SPDX-License-Identifier: Apache-2.0
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 from django.db import transaction
 from django.conf import settings
@@ -10,6 +8,7 @@ from django.core.cache import cache
 from .. import redis_id
 from ..models import DomainTestReport, MailTestReport
 from ..batch import util
+from internetnl.celery import app
 
 logger = get_task_logger(__name__)
 
@@ -174,7 +173,7 @@ def _update_hof():
 
 # Disable HoF when on batch mode, too much DB activity.
 if not settings.ENABLE_BATCH:
-    @periodic_task(run_every=(crontab(hour="*", minute="*/10", day_of_week="*")))
+    @app.task(name='update_HoF_ranking')
     def ranking():
         lock_id = redis_id.hof_lock.id
         lock_ttl = redis_id.hof_lock.ttl

internetnl/celery.py
@@ -3,6 +3,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internetnl.settings')
 
@@ -19,3 +20,19 @@ app.backend.result_consumer.start("")
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+if app.conf.ENABLE_BATCH:
+    app.conf.beat_schedule = {
+        'run_batch': {
+                'task': 'tasks.run_batch',
+                'schedule': settings.BATCH_SCHEDULER_INTERVAL,
+        }
+    }
+else:
+    # Disable HoF when on batch mode, too much DB activity.
+    app.conf.beat_schedule = {
+        'generate_HoF': {
+                'task': 'update_HoF_ranking',
+                'schedule': crontab(hour='*', minute='*/10', day_of_week='*'),
+        }
+    }

commit 1ba3bfdff2a6c1d50ae14e0221aef23c3db30ad6

celery: Improve consistency of (beat) scheduled tasks
This commit makes several changes to improve consistency:
  - rename `(checks.tasks.update.)ranking` to `update_hof`
  - refer to all tasks by their fully qualified path
  - configure schedule intervals in `internetnl/settings.py`
  - unconditionally define tasks, but only enable them when necessary in
    the scheduler. Ie. remove `if ENABLE_BATCH:` conditionals.

Fixes: 4407156ba9391cd437e44095dba938f7c719f233

Signed-off-by: Maarten Aertsen <maarten.aertsen@ncsc.nl>

checks/batch/scheduler.py
@@ -582,17 +582,16 @@ def _run_scheduler():
     logger.info("Found {} domains".format(found_domains))
 
 
-if settings.ENABLE_BATCH:
-    @app.task(name='run_batch')
-    def run():
-        """
-        Run the scheduler every interval only if it is not running already.
+@batch_shared_task
+def run():
+    """
+    Run the scheduler every interval only if it is not running already.
 
-        """
-        lock_id = redis_id.batch_scheduler_lock.id
-        lock_ttl = redis_id.batch_scheduler_lock.ttl
-        with util.memcache_lock(lock_id, lock_ttl) as acquired:
-            if acquired:
-                _run_scheduler()
-                return
-        logger.info("Already running...")
+    """
+    lock_id = redis_id.batch_scheduler_lock.id
+    lock_ttl = redis_id.batch_scheduler_lock.ttl
+    with util.memcache_lock(lock_id, lock_ttl) as acquired:
+        if acquired:
+            _run_scheduler()
+            return
+    logger.info("Already running...")

checks/tasks/update.py
@@ -8,7 +8,7 @@ from django.core.cache import cache
 from .. import redis_id
 from ..models import DomainTestReport, MailTestReport
 from ..batch import util
-from internetnl.celery import app
+from celery import shared_task
 
 logger = get_task_logger(__name__)
 
@@ -171,12 +171,10 @@ def _update_hof():
             cache.set(cache_id, cached_data, cache_ttl)
 
 
-# Disable HoF when on batch mode, too much DB activity.
-if not settings.ENABLE_BATCH:
-    @app.task(name='update_HoF_ranking')
-    def ranking():
-        lock_id = redis_id.hof_lock.id
-        lock_ttl = redis_id.hof_lock.ttl
-        with util.memcache_lock(lock_id, lock_ttl) as acquired:
-            if acquired:
-                _update_hof()
+@shared_task
+def update_hof():
+    lock_id = redis_id.hof_lock.id
+    lock_ttl = redis_id.hof_lock.ttl
+    with util.memcache_lock(lock_id, lock_ttl) as acquired:
+        if acquired:
+            _update_hof()

internetnl/celery.py
@@ -20,15 +20,15 @@ def debug_task(self):
 if app.conf.ENABLE_BATCH:
     app.conf.beat_schedule = {
         'run_batch': {
-                'task': 'tasks.run_batch',
-                'schedule': app.conf.BATCH_SCHEDULER_INTERVAL,
+                'task': 'checks.batch.scheduler.run',
+                'schedule': app.conf.BATCH_SCHEDULER_INTERVAL
         }
     }
 else:
     # Disable HoF when on batch mode, too much DB activity.
     app.conf.beat_schedule = {
         'generate_HoF': {
-                'task': 'update_HoF_ranking',
-                'schedule': crontab(hour='*', minute='*/10', day_of_week='*'),
+                'task': 'checks.tasks.update.update_hof',
+                'schedule': app.conf.HOF_UPDATE_INTERVAL
         }
     }

internetnl/settings.py-dist
@@ -262,7 +262,7 @@ CELERY_TASK_ROUTES = {
 
         'checks.views.shared.run_stats_queries': {'queue': 'slow_db_worker'},
         'checks.views.shared.update_running_status': {'queue': 'slow_db_worker'},
-        'checks.tasks.update.ranking': {'queue': 'slow_db_worker'},
+        'checks.tasks.update.update_hof': {'queue': 'slow_db_worker'},
 }
 
 # Shared task timings
@@ -335,6 +335,8 @@ MATOMO_SITEID = "site_id"
 # Used for subdomain tracking eg. *.internet.nl
 MATOMO_SUBDOMAIN_TRACKING = ""
 
+# --- HoF update interval
+HOF_UPDATE_INTERVAL = 600 # seconds
 
 # --- Extra manual HoF page(s)
 #