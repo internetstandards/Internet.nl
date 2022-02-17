5dc27b71741a01ab9ef054dd7c975b80e8f20f2e
stitch1 <elger.jonker@gmail.com>
2022-02-17 08:40:38 +0100

    Allow running of separate probes for deeper inspection

--- interface/management/commands/probe.py
+++ interface/management/commands/probe.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict, Callable, Optional
 
 from django.core.management.base import BaseCommand
 
@@ -6,14 +7,16 @@ from checks.tasks import ipv6, dnssec, mail, shared, appsecpriv, tls
 
 
 def force_debug_logging():
-    log = logging.getLogger(__package__)
+    debug_log = logging.getLogger(__package__)
 
-    formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+    formatter = logging.Formatter(
+        "%(asctime)s\t%(levelname)-8s - %(filename)-20s:%(lineno)-4s - %(funcName)20s() - %(message)s"
+    )
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     stream_handler.setLevel(logging.DEBUG)
-    log.addHandler(stream_handler)
-    log.setLevel(logging.DEBUG)
+    debug_log.addHandler(stream_handler)
+    debug_log.setLevel(logging.DEBUG)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
     logger = logging.getLogger("internetnl")
@@ -23,12 +26,12 @@ def force_debug_logging():
     logger = logging.getLogger("celery")
     logger.setLevel(logging.DEBUG)
 
-    return log
+    return debug_log
 
 
 log = force_debug_logging()
 
-probe_mapping = {
+PROBES: Dict[str, Optional[Callable]] = {
     "ipv6_web": ipv6.web,
     "ipv6_ns": ipv6.ns,
     "ipv6_mx": ipv6.mx,
@@ -44,6 +47,7 @@ probe_mapping = {
     "tls_web_conn": tls.web_conn,
     "tls_web_http": tls.web_http,
     "tls_mail_smtp_starttls": tls.mail_smtp_starttls,
+    "all": None,
 }
 
 
@@ -58,30 +62,45 @@ class Command(BaseCommand):
     help = "Launch a probe and retrieve the results. This allows for easier debugging of probes."
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--domain",
-            type=str,
-            default="internet.nl",
-            nargs="?",
-        )
-
-        parser.add_argument("--probe", type=str, default="ipv6_web", nargs="?", choices=probe_mapping.keys())
+        parser.add_argument("--domain", type=str, default="internet.nl", nargs="?")
+        parser.add_argument("--probe", type=str, default="ipv6_web", nargs="?", choices=PROBES.keys())
 
     def handle(self, *args, **options):
         domain = options.get("domain", "internet.nl")
         probe = options.get("probe", "ipv6_web")
-        log.info(f"Performing {probe} on {domain}.")
 
-        if probe in ["tls_web_cert", "tls_web_conn", "tls_web_http"]:
-            # todo: retrieve af ip pairs as first argument
-            af_ip_pairs = ""
-            return_value = probe_mapping[probe](af_ip_pairs, domain)
-        if probe in ["tls_mail_smtp_starttls"]:
-            # todo: retrieve mailservers as first argument
-            mailservers = ""
-            return_value = probe_mapping[probe](mailservers, domain)
+        if probe == "all":
+            run_all_probes(domain)
         else:
-            return_value = probe_mapping[probe](domain)
+            run_probe(probe, domain)
 
-        log.info(f"Retrieved return value: {return_value}")
-        log.info("Done")
+
+def run_all_probes(domain):
+    log.info(f"Performing all probes on {domain}.")
+    for probe_name in PROBES.keys():
+        if probe_name == "all":
+            continue
+        run_probe(probe_name, domain)
+    log.info(f"Done with all probes on {domain}.")
+
+
+def run_probe(probe: str, domain: str):
+    log.info(f"Performing {probe} on {domain}.")
+
+    if probe in ["tls_web_cert", "tls_web_conn", "tls_web_http", "appsecpriv_web_appsecpriv"]:
+        log.debug("First retrieving af_ip_pairs")
+        af_ip_pairs = shared.resolve_a_aaaa(domain)
+        log.debug(f"af_ip_pairs retrieved: {af_ip_pairs}")
+        return_value = PROBES[probe](af_ip_pairs, domain)
+
+    elif probe in ["tls_mail_smtp_starttls", "dnssec_mail_is_secure"]:
+        log.debug("First retrieving mailservers")
+        mailservers = shared.mail_get_servers(domain)
+        log.debug(f"Mailservers retrieved: {mailservers}")
+        return_value = PROBES[probe](mailservers, domain)
+
+    else:
+        return_value = PROBES[probe](domain)
+
+    log.info(f"Retrieved return value: {return_value}")
+    log.info("Done")
