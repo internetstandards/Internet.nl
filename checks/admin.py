9ccf5bfc2843f1bc773d45658ff8bb3919f3e356
stitch1 <elger.jonker@gmail.com>
2022-02-17 10:29:01 +0100

    add admin features to ipv6 tests

--- checks/admin.py
+++ checks/admin.py
@@ -2,52 +2,52 @@
 # SPDX-License-Identifier: Apache-2.0
 from django.contrib import admin
 
-from checks.models import (
-    ASRecord,
-    AutoConf,
-    BatchDomain,
-    BatchMailTest,
-    BatchRequest,
-    BatchUser,
-    BatchWebTest,
-    ConnectionTest,
-    DomainTestAppsecpriv,
-    DomainTestDnssec,
-    DomainTestIpv6,
-    DomainTestReport,
-    DomainTestTls,
-    MailTestAuth,
-    MailTestDnssec,
-    MailTestIpv6,
-    MailTestReport,
-    MailTestTls,
-    MxDomain,
-    NsDomain,
-    Resolver,
-    WebDomain,
-    WebTestAppsecpriv,
-)
+from checks import models
 
-admin.site.register(ConnectionTest)
-admin.site.register(Resolver)
-admin.site.register(ASRecord)
-admin.site.register(DomainTestIpv6)
-admin.site.register(WebDomain)
-admin.site.register(MailTestTls)
-admin.site.register(MailTestDnssec)
-admin.site.register(DomainTestDnssec)
-admin.site.register(DomainTestTls)
-admin.site.register(WebTestAppsecpriv)
-admin.site.register(DomainTestAppsecpriv)
-admin.site.register(DomainTestReport)
-admin.site.register(MailTestIpv6)
-admin.site.register(NsDomain)
-admin.site.register(MxDomain)
-admin.site.register(MailTestAuth)
-admin.site.register(MailTestReport)
-admin.site.register(BatchUser)
-admin.site.register(BatchRequest)
-admin.site.register(BatchDomain)
-admin.site.register(BatchWebTest)
-admin.site.register(BatchMailTest)
-admin.site.register(AutoConf)
+admin.site.register(models.ConnectionTest)
+admin.site.register(models.Resolver)
+admin.site.register(models.ASRecord)
+
+admin.site.register(models.WebDomain)
+admin.site.register(models.MailTestTls)
+admin.site.register(models.MailTestDnssec)
+admin.site.register(models.DomainTestDnssec)
+admin.site.register(models.DomainTestTls)
+admin.site.register(models.WebTestAppsecpriv)
+admin.site.register(models.DomainTestAppsecpriv)
+admin.site.register(models.DomainTestReport)
+admin.site.register(models.NsDomain)
+admin.site.register(models.MxDomain)
+admin.site.register(models.MailTestAuth)
+admin.site.register(models.MailTestReport)
+admin.site.register(models.BatchUser)
+admin.site.register(models.BatchRequest)
+admin.site.register(models.BatchDomain)
+admin.site.register(models.BatchWebTest)
+admin.site.register(models.BatchMailTest)
+admin.site.register(models.AutoConf)
+
+
+@admin.register(models.DomainTestIpv6)
+class DomainTestIpv6Admin(admin.ModelAdmin):
+    list_display = [
+        "timestamp",
+        "domain",
+        "report",
+        "web_simhash_distance",
+        "web_simhash_score",
+        "web_score",
+        "mx_score",
+        "ns_score",
+        "score",
+        "max_score",
+    ]
+    search_fields = ["domain", "report"]
+    list_filter = ["timestamp"]
+
+
+@admin.register(models.MailTestIpv6)
+class MailTestIpv6(admin.ModelAdmin):
+    list_display = ["timestamp", "domain", "report", "mx_score", "ns_score", "score", "max_score", "mx_status"]
+    search_fields = ["domain", "report"]
+    list_filter = ["timestamp"]
