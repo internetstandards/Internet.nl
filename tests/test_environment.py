6c053a29e16b23263565a9a296fb47b548baabb0
stitch1 <elger.jonker@gmail.com>
2022-02-28 08:53:19 +0100

    add a trivial redis connection test

---
+++ tests/test_environment.py
@@ -0,0 +1,12 @@
+from django_redis import get_redis_connection
+
+
+def test_redis_connection():
+    # Sends a few trivial queries to redis, to see if the build can talk to it without infinite waits
+    red = get_redis_connection("default")
+    red.set("testing", 123)
+    red.expire("testing", 2)
+    assert red.get("testing") == b"123"
+
+    red.expire("testing", 0)
+    assert red.scard("testing") == 0
