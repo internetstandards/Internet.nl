a82ad1a75f637fa84b0741fd12ab491e683402ed
Sasha Romijn <github@mxsasha.eu>
2022-08-23 11:10:33 +0200

    Update copyright headers to reflect current authorship (#732)

--- checks/tasks/dmarc_parser.py
+++ checks/tasks/dmarc_parser.py
@@ -1,4 +1,4 @@
-# Copyright: 2019, NLnet Labs and the Internet.nl contributors
+# Copyright: 2022, ECP, NLnet Labs and the Internet.nl contributors
 # SPDX-License-Identifier: Apache-2.0
 from urllib.parse import urlparse
 
--- docker/postgres-ping.sh
+++ docker/postgres-ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright: 2021, NLnet Labs and the Internet.nl contributors
+# Copyright: 2022, ECP, NLnet Labs and the Internet.nl contributors
 # SPDX-License-Identifier: Apache-2.0
 
 set -eu
