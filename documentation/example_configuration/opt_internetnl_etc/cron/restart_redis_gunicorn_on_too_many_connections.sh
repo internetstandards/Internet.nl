56b95dd2129a53bbf4df63c0e48a4577339e58c3
Sasha Romijn <github@mxsasha.eu>
2023-02-27 20:10:52 +0100

    update

--- documentation/example_configuration/opt_internetnl_etc/cron/restart_redis_gunicorn_on_too_many_connections.sh
+++ documentation/example_configuration/opt_internetnl_etc/cron/restart_redis_gunicorn_on_too_many_connections.sh
@@ -4,9 +4,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 REDIS_CONS=$(netstat -antlp | grep :6379 | grep "CLOSE_WAIT" | wc -l)
 
-if [ ${REDIS_CONS} -gt 60000 ]; then
-	logger --tag redis-connections-limit-cron "Redis is having too much connections in CLOSE_WAIT state. Restarting internetnl batch services and redis."
-	for i in $(ls -1 /etc/systemd/system/internetnl-batch*.service); do /bin/systemctl restart `basename $i`; done
-	/bin/systemctl restart redis-server
+if [ ${REDIS_CONS} -gt 50000 ]; then
+    logger --tag redis-connections-limit-cron "Redis is having too much connections in CLOSED state. Restarting internetnl batch services"
+    for i in $(ls -1 /etc/systemd/system/internetnl-batch*.service); do /bin/systemctl restart `basename $i`; done
 fi
-
