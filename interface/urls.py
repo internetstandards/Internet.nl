e726402e1b55434ce32624076072c15308933936
Johan Bloemberg <git@ijohan.nl>
2023-03-07 12:00:31 +0100

    Miscellaneous cleanup

--- interface/urls.py
+++ interface/urls.py
@@ -127,10 +127,6 @@ if settings.ENABLE_BATCH is True:
         url(r"^api/batch/", batch.old_url, name="batch_old"),
     ]
 
-
-if settings.DEBUG is True:
-    pass
-
 # Static URLs, these are normally never exposed on production environment as
 # they are served by the webserver upfront. Since they are not accessed, it
 # does not matter to leave them in.
