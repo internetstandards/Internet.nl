fb38c773a1dc9e7c9d19a84f366d5f39e9f68cee
Johan Bloemberg <git@ijohan.nl>
2023-03-21 15:13:39 +0100

    Allow local development in Docker Compose

--- internetnl/settings-dist.py
+++ internetnl/settings-dist.py
@@ -270,7 +270,7 @@ LANGUAGES = sorted(
 #     https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "interface/assets"),
 ]
