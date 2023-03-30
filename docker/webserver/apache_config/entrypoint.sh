45e07621322b0bfce81c44489f8323ee68538b81
Johan Bloemberg <git@ijohan.nl>
2023-03-30 20:44:12 +0200

    Httpd webserver for testing against current configuration, to be replaced with nginx config

---
+++ docker/webserver/apache_config/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+echo "Define servername ${INTERNETNL_DOMAINNAME}" > sites-enabled/00_variables.conf
+echo "Define ipv6_test_addr ${IPV6_TEST_ADDR}" >> sites-enabled/00_variables.conf
+
+httpd-foreground -c"Include /usr/local/apache2/sites-enabled/*.conf" "$@"
