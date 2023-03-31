2c794fd961ab7bf3eebd18968b8dfff18bdea350
Johan Bloemberg <git@ijohan.nl>
2023-03-31 10:25:19 +0200

    WIP: temporary letsencrypt TLS certs

--- docker/webserver/apache_config/entrypoint.sh
+++ docker/webserver/apache_config/entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 echo "Define servername ${INTERNETNL_DOMAINNAME}" > sites-enabled/00_variables.conf
 echo "Define ipv6_test_addr ${IPV6_TEST_ADDR}" >> sites-enabled/00_variables.conf
+test -f /etc/acme/certs/cert.pem && echo "Define enable_tls true" >> sites-enabled/00_variables.conf
 
 httpd-foreground -c"Include /usr/local/apache2/sites-enabled/*.conf" "$@"
