0b7ca5ffefd1e02b9b70186b4da07a037edde9da
Sasha Romijn <github@mxsasha.eu>
2023-04-03 16:22:17 +0200

    more updates

--- checks/http_client.py
+++ checks/http_client.py
@@ -10,7 +10,9 @@ import urllib3
 from forcediphttpsadapter.adapters import ForcedIPHTTPSAdapter
 
 from checks.tasks import SetupUnboundContext
+from checks.tasks.tls_connection import DEFAULT_TIMEOUT
 from checks.tasks.tls_connection_exceptions import NoIpError
+from interface.views.shared import ub_resolve_with_timeout
 from internetnl import log
 
 # Disable HTTPS warnings as we intentionally disable HTTPS verification
@@ -21,9 +23,9 @@ def http_get(
     url: str, headers: Optional[Dict] = None, session: Optional[requests.Session] = None, *args, **kwargs
 ) -> requests.Response:
     """
-    Perform a HTTP GET request using the stored session
+    Perform a standard HTTP GET request. If session is given, it is used.
+    Other (kw)args are passed to requests.get.
     """
-    # TODO: auto-retry
     start_time = timer()
 
     if not headers:
@@ -32,7 +34,16 @@ def http_get(
     if not session:
         session = requests.session()
 
-    response = session.get(url, headers=headers, stream=True, *args, **kwargs)
+    try:
+        response = session.get(url, headers=headers, stream=True, *args, **kwargs)
+    except requests.RequestException:
+        # Retry, once, then log and raise the exception
+        try:
+            response = session.get(url, headers=headers, stream=True, *args, **kwargs)
+        except requests.RequestException as exc:
+            log.debug(f"HTTP request raised exception: {url} (headers: {headers})", exc_info=exc)
+            raise
+
     log.debug(f"HTTP request completed in {timer()-start_time:.06f}s: {url} (headers: {headers})")
     return response
 
@@ -47,6 +58,12 @@ def http_get_ip(
     *args,
     **kwargs,
 ) -> requests.Response:
+    """
+    Perform an HTTP GET with the given parameters, while forcing the destination IP
+    to a particular IP that may not match the hostname.
+    TLS certificate verification is always disabled.
+    Other (kw)args are passed to requests.get.
+    """
     path = path.lstrip("/")
     if not headers:
         headers = {}
@@ -66,10 +83,16 @@ def http_get_ip(
 def http_get_af(
     hostname: str, port: int, af: socket.AddressFamily, task: Optional[SetupUnboundContext] = None, *args, **kwargs
 ) -> requests.Response:
+    """
+    Perform an HTTP GET request to the given hostname/port, restricting to a certain address family.
+    Other (kw)args are passed to requests.get.
+    """
     rr_type = unbound.RR_TYPE_AAAA if af == socket.AF_INET6 else unbound.RR_TYPE_A
-    # cb_data = ub_resolve_with_timeout(host, rr_type, unbound.RR_CLASS_IN, timeout)
-    # ips = [socket.inet_ntop(af, rr) for rr in cb_data["data"].data]
-    ips = task.resolve(hostname, rr_type)
+    if task:
+        ips = task.resolve(hostname, rr_type)
+    else:
+        cb_data = ub_resolve_with_timeout(hostname, rr_type, unbound.RR_CLASS_IN, DEFAULT_TIMEOUT)
+        ips = [socket.inet_ntop(af, rr) for rr in cb_data["data"].data]
     exc = NoIpError(f"Unable to resolve {rr_type} record for host '{hostname}'")
     for ip in ips:
         try:
