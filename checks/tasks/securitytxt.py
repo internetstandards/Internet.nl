internetstandards/Internet.nl

commit f8358c4d7ee8b191f4859e9b8899eef5535004c9

Fix #772 - Evaluate Canonical value of security.txt

checks/tasks/securitytxt.py
@@ -23,6 +23,7 @@ class SecuritytxtRetrieveResult:
     content: Optional[str]
     url: str
     found_host: str
+    found_url: str
     errors: List[Dict[str, str]]
 
 
@@ -61,12 +62,24 @@ def _retrieve_securitytxt(af_ip_pair, hostname: str, task: SetupUnboundContext) 
     except requests.RequestException:
         return _evaluate_response(None, None, hostname, path, "", hostname)
     return _evaluate_response(
-        response.status_code, response.headers.get("Content-Type", ""), hostname, path, content, found_host
+        response.status_code,
+        response.headers.get("Content-Type", ""),
+        hostname,
+        path,
+        content,
+        found_host,
+        response.url,
     )
 
 
 def _evaluate_response(
-    status: Optional[int], content_type: Optional[str], domain: str, path: str, content: str, found_host: str
+    status: Optional[int],
+    content_type: Optional[str],
+    domain: str,
+    path: str,
+    content: str,
+    found_host: str,
+    found_url: str,
 ) -> SecuritytxtRetrieveResult:
     errors = []
     media_type, charset = None, None
@@ -108,6 +121,7 @@ def _evaluate_response(
         content=content,
         url=f"https://{domain}{path}",
         found_host=found_host,
+        found_url=found_url,
         errors=errors,
     )
 
@@ -125,8 +139,7 @@ def _evaluate_securitytxt(result: SecuritytxtRetrieveResult):
             "securitytxt_recommendations": [],
         }
 
-    # URL intentionally not passed as Canonical testing is out of scope at this time
-    parser = sectxt.Parser(result.content)
+    parser = sectxt.Parser(result.content, urls=result.found_url)
 
     errors = result.errors + parser_format(parser.errors)
     score = scoring.WEB_APPSECPRIV_SECURITYTXT_BAD if errors else scoring.WEB_APPSECPRIV_SECURITYTXT_GOOD

checks/test/test_securitytxt.py
@@ -26,8 +26,9 @@ def test_evaluate_response():
         path=SECURITYTXT_EXPECTED_PATH,
         content=sectxt_content,
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
     ):
-        return _evaluate_response(status, content_type, domain, path, content, found_host)
+        return _evaluate_response(status, content_type, domain, path, content, found_host, found_url)
 
     result = _evaluate_with_valid_defaults()
     assert result == SecuritytxtRetrieveResult(
@@ -35,6 +36,7 @@ def test_evaluate_response():
         content=sectxt_content,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[],
     )
 
@@ -46,6 +48,7 @@ def test_evaluate_response():
         content=sectxt_content,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "no_security_txt_404"}],
     )
 
@@ -57,6 +60,7 @@ def test_evaluate_response():
         content=sectxt_content,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "no_security_txt_other", "context": {"status_code": 500}}],
     )
 
@@ -68,6 +72,7 @@ def test_evaluate_response():
         content=None,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "no_content_type"}],
     )
 
@@ -79,6 +84,7 @@ def test_evaluate_response():
         content=None,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "invalid_media"}],
     )
 
@@ -90,6 +96,7 @@ def test_evaluate_response():
         content=None,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "invalid_media"}],
     )
 
@@ -101,6 +108,7 @@ def test_evaluate_response():
         content=sectxt_content,
         url="https://example.com/.well-known/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "invalid_charset"}],
     )
 
@@ -112,6 +120,7 @@ def test_evaluate_response():
         content=sectxt_content,
         url="https://example.com/security.txt",
         found_host="example.nl",
+        found_url="https://example.nl/.well-known/security.txt",
         errors=[{"msgid": "location"}],
     )
 
@@ -122,6 +131,7 @@ def test_evaluate_securitytxt():
         content="",
         url="https://example.com/security.txt",
         found_host="host",
+        found_url="https://host/.well-known/security.txt",
         errors=[{"msgid": "example"}],
     )
     assert _evaluate_securitytxt(result) == {
@@ -137,6 +147,7 @@ def test_evaluate_securitytxt():
         content="invalid content",
         url="https://example.com/security.txt",
         found_host="host",
+        found_url="https://host/.well-known/security.txt",
         errors=[],
     )
     assert _evaluate_securitytxt(result) == {
@@ -157,6 +168,7 @@ def test_evaluate_securitytxt():
         content="Expires: 2050-09-01T00:00:00.000Z\nContact: mailto:security@example.com\n",
         url="https://example.com/security.txt",
         found_host="host",
+        found_url="https://host/.well-known/security.txt",
         errors=[{"msgid": "example"}],
     )
     assert _evaluate_securitytxt(result) == {
@@ -173,9 +185,38 @@ def test_evaluate_securitytxt():
 
     result = SecuritytxtRetrieveResult(
         found=True,
-        content="Expires: 2050-09-01T00:00:00.000Z\nContact: mailto:security@example.com\n",
+        content=(
+            "Expires: 2050-09-01T00:00:00.000Z\n"
+            "Contact: mailto:security@example.com\n"
+            "Canonical: https://host-other/.well-known/security.txt\n"
+        ),
         url="https://example.com/security.txt",
         found_host="host",
+        found_url="https://host/.well-known/security.txt",
+        errors=[],
+    )
+    assert _evaluate_securitytxt(result) == {
+        "securitytxt_enabled": True,
+        "securitytxt_score": scoring.WEB_APPSECPRIV_SECURITYTXT_GOOD,
+        "securitytxt_found_host": "host",
+        "securitytxt_errors": [{"msgid": "no_canonical_match", "context": {"line_no": None}}],
+        "securitytxt_recommendations": [
+            {"msgid": "long_expiry", "context": {"line_no": 1}},
+            {"msgid": "no_encryption", "context": {"line_no": None}},
+            {"msgid": "not_signed", "context": {"line_no": None}},
+        ],
+    }
+
+    result = SecuritytxtRetrieveResult(
+        found=True,
+        content=(
+            "Expires: 2050-09-01T00:00:00.000Z\n"
+            "Contact: mailto:security@example.com\n"
+            "Canonical: https://host/.well-known/security.txt\n"
+        ),
+        url="https://example.com/security.txt",
+        found_host="host",
+        found_url="https://host/.well-known/security.txt",
         errors=[],
     )
     assert _evaluate_securitytxt(result) == {

commit 0140445fe927be31627444d9104ec9cf8c83da6b

Fix security.txt test for connection errors (ref #772)
Bug introduced in f8358c4d7ee8b191f4859e9b8899eef5535004c9

checks/tasks/securitytxt.py
@@ -23,7 +23,7 @@ class SecuritytxtRetrieveResult:
     content: Optional[str]
     url: str
     found_host: str
-    found_url: str
+    found_url: Optional[str]
     errors: List[Dict[str, str]]
 
 
@@ -60,7 +60,7 @@ def _retrieve_securitytxt(af_ip_pair, hostname: str, task: SetupUnboundContext) 
             errors=[{"msgid": "utf8"}],
         )
     except requests.RequestException:
-        return _evaluate_response(None, None, hostname, path, "", hostname)
+        return _evaluate_response(None, None, hostname, path, "", hostname, None)
     return _evaluate_response(
         response.status_code,
         response.headers.get("Content-Type", ""),
@@ -79,7 +79,7 @@ def _evaluate_response(
     path: str,
     content: str,
     found_host: str,
-    found_url: str,
+    found_url: Optional[str],
 ) -> SecuritytxtRetrieveResult:
     errors = []
     media_type, charset = None, None