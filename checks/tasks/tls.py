642ef401018d2e942fec58327132504cf148a66e
Sasha Romijn <github@mxsasha.eu>
2023-05-02 16:36:48 +0200

    Switch TLS to sslyze/nassl based reimplementation

--- checks/tasks/tls.py
+++ checks/tasks/tls.py
@@ -38,7 +38,7 @@ from nassl import _nassl
 from nassl.ocsp_response import OcspResponseNotTrustedError
 
 from checks import categories, scoring
-from checks.caa.retrieval import retrieve_parse_caa, CAA_MSGID_INSUFFICIENT_POLICY
+from checks.caa.retrieval import retrieve_parse_caa
 from checks.http_client import http_get_ip
 from checks.models import (
     CipherOrderStatus,
@@ -656,8 +656,7 @@ def save_results(model, results, addr, domain, category):
                 model.cert_hostmatch_score = result.get("hostmatch_score")
                 model.cert_hostmatch_bad = result.get("hostmatch_bad")
                 model.caa_enabled = result.get("caa_result").caa_found
-                model.caa_records = result.get("caa_result").caa_records_str
-                model.caa_errors = [ttti.to_dict() for ttti in result.get("caa_result").errors]
+                model.caa_error = [ttti.to_dict() for ttti in result.get("caa_result").errors]
                 model.caa_recommendations = [ttti.to_dict() for ttti in result.get("caa_result").recommendations]
                 model.caa_score = result.get("caa_result").score
                 model.caa_found_on_domain = result.get("caa_result").canonical_name
@@ -730,8 +729,7 @@ def save_results(model, results, addr, domain, category):
                     model.cert_hostmatch_score = result.get("hostmatch_score")
                     model.cert_hostmatch_bad = result.get("hostmatch_bad")
                     model.caa_enabled = result.get("caa_result").caa_found
-                    model.caa_records = result.get("caa_result").caa_records_str
-                    model.caa_errors = [ttti.to_dict() for ttti in result.get("caa_result").errors]
+                    model.caa_error = [ttti.to_dict() for ttti in result.get("caa_result").errors]
                     model.caa_recommendations = [ttti.to_dict() for ttti in result.get("caa_result").recommendations]
                     model.caa_score = result.get("caa_result").score
                     model.caa_found_on_domain = result.get("caa_result").canonical_name
@@ -903,15 +901,10 @@ def build_report(dttls, category):
                 caa_tech_table = caa_host_message + dttls.caa_errors + dttls.caa_recommendations
                 for record in dttls.caa_records:
                     caa_tech_table.append(
-                        TranslatableTechTableItem(msgid="caa-record", context={"record": record}).to_dict()
+                        TranslatableTechTableItem(msgid="caa_record", context={"record": record}).to_dict()
                     )
                 if not dttls.caa_enabled:
                     category.subtests["web_caa"].result_bad(caa_tech_table)
-                elif dttls.caa_errors:
-                    if all([error["msgid"] != CAA_MSGID_INSUFFICIENT_POLICY for error in dttls.caa_errors]):
-                        category.subtests["web_caa"].result_syntax_error(caa_tech_table)
-                    else:
-                        category.subtests["web_caa"].result_insufficient(caa_tech_table)
                 elif dttls.caa_recommendations:
                     category.subtests["web_caa"].result_recommendations(caa_tech_table)
                 else:
@@ -1082,15 +1075,10 @@ def build_report(dttls, category):
             caa_tech_table = caa_host_message + dttls.caa_errors + dttls.caa_recommendations
             for record in dttls.caa_records:
                 caa_tech_table.append(
-                    TranslatableTechTableItem(msgid="caa-record", context={"record": record}).to_dict()
+                    TranslatableTechTableItem(msgid="caa_record", context={"record": record}).to_dict()
                 )
             if not dttls.caa_enabled:
                 category.subtests["mail_caa"].result_bad(caa_tech_table)
-            elif dttls.caa_errors:
-                if all([error["msgid"] != CAA_MSGID_INSUFFICIENT_POLICY for error in dttls.caa_errors]):
-                    category.subtests["mail_caa"].result_syntax_error(caa_tech_table)
-                else:
-                    category.subtests["mail_caa"].result_insufficient(caa_tech_table)
             elif dttls.caa_recommendations:
                 category.subtests["mail_caa"].result_recommendations(caa_tech_table)
             else:
