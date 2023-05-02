24a24ab9b052025c60020c9cbd4c16a3114444b1
Sasha Romijn <github@mxsasha.eu>
2023-05-02 16:36:48 +0200

    Switch TLS to sslyze/nassl based reimplementation

--- remote_data/certs/update-certs.sh
+++ remote_data/certs/update-certs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ./mk-ca-bundle.pl -t -s SHA1 -d release -f
+perl -i'' -pe 's/[^[:ascii:]]//g' ca-bundle.crt
 
 echo "Extract SHA1 fingerprints"
 grep "SHA1 Fingerprint" ca-bundle.crt | cut -d "=" -f 2 > root_fingerprints.tmp
