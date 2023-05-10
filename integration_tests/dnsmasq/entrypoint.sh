95445f08bf4770de373481325a4794b325d70c59
Johan Bloemberg <git@ijohan.nl>
2023-05-10 19:54:51 +0200

    Further isolate DNS in test environment to speed up testing
    Disable forwarding of all DNS requests not explicitly handled by dnsmasq so tests won't wait for resolving. This speeds up the test suite from minutes to seconds.

--- integration_tests/dnsmasq/entrypoint.sh
+++ integration_tests/dnsmasq/entrypoint.sh
@@ -3,7 +3,10 @@
 set -e
 
 cat >/etc/dnsmasq.d/connection-test.conf <<EOF
-address=/internet.nl/192.0.2.1
+address=/$INTERNETNL_DOMAINNAME/$IPV4_WEBSERVER_IP_PUBLIC
+address=/$TEST_TARGET_DOMAINNAME/$IPV4_IP_TEST_TARGET_PUBLIC
+address=/$TEST_TARGET_DOMAINNAME/$IPV6_IP_TEST_TARGET_PUBLIC
+address=/invalid-domain.example.com/
 server=/test-ns-signed.internet.nl/$IPV4_IP_UNBOUND_INTERNAL
 server=/test-ns6-signed.internet.nl/$IPV4_IP_UNBOUND_INTERNAL
 txt-record=0.43.172.origin.asn.cymru.com,"21928 | 172.32.0.0/11 | US | arin | 2012-09-18"
@@ -11,6 +14,8 @@ txt-record=1.0.0.0.3.4.0.0.0.0.d.f.origin6.asn.cymru.com,""
 mx-host=test-target.internet.nl,mx.test-target.internet.nl,10
 log-queries
 log-facility=-
+# don't forward resolving to speed up tests
+no-resolv
 EOF
 
 dnsmasq -k
