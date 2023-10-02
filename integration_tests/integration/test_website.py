7a089cdea88265a5f25bf291b1a9fe6452dbe6ea
Johan Bloemberg <git@ijohan.nl>
2023-10-02 11:58:16 +0200

    Fix ipv6 nameserver test
    This unbound context is seperate from 'normal' resolving contexts in that it configures the NS to test as forwarding address. Hence it should not use the internal resolver.

--- integration_tests/integration/test_website.py
+++ integration_tests/integration/test_website.py
@@ -53,3 +53,23 @@ def test_your_website_probe_success(page, app_url, probe, test_domain):
 
     probe_result = page.locator(f"#site{probe}-results")
     expect(probe_result).to_have_class("passed")
+
+
+def test_ipv6_ns_with_bad_connectivity(page, app_url, unique_id):
+    """Test if a target with a unresponsive IPv6 nameserver returns the correct result."""
+
+    test_domain = f"{unique_id}.bad-ipv6-ns.test"
+
+    page.goto(f"{app_url}/site/{test_domain}/")
+    # make sure the test has been started
+    page.get_by_text(f"Website test: {test_domain}")
+    # make sure the test is completed
+    page.wait_for_url(f"{app_url}/site/{test_domain}/*/")
+
+    print_results_url(page)
+
+    # test results should indicate ipv6 ns are found
+    expect(page.get_by_text("Two or more name servers of your domain have an IPv6 address."))
+
+    # but some of them are not resolvable
+    expect(page.get_by_text("Not all name servers that have an IPv6 address are reachable over IPv6."))
