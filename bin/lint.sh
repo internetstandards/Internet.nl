4230edf9215521e36f5f27ca3b7227308a095346
Johan Bloemberg <git@ijohan.nl>
2023-11-10 15:11:45 +0100

    Add shellcheck for shell scripts

--- bin/lint.sh
+++ bin/lint.sh
@@ -8,6 +8,7 @@ fail=0
 
 pylama --skip "**/migrations/*" ${@} || fail=1
 black --line-length 120 --check ${@} || fail=1
+shellcheck -e SC1071 docker/cron/periodic/*/* || fail=1
 
 SKIP_SECRET_KEY_CHECK=True CACHE_LOCATION= ENABLE_BATCH= ./manage.py makemigrations --noinput --check --dry-run || fail=1
 
