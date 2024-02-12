d5bc740f5adc0dbd4d4d172148b0b949e62011e6
Benjamin W. Broersma <bw@broersma.com>
2024-02-12 20:45:56 +0100

    [1.8.x] Fix certbot subdomains (fixes #1275)
    (cherry picked from commit f077e64e93ced678c8a31779727a3a93cb07d32d)

--- docker/webserver/certbot.sh
+++ docker/webserver/certbot.sh
@@ -15,7 +15,10 @@ else
 fi
 
 domain=$INTERNETNL_DOMAINNAME
-subdomains="nl.$domain,en.$domain,www.$domain,ipv6.$domain,conn.$domain,en.conn.$domain,nl.conn.$domain,www.conn.$domain"
+subdomains="nl.$domain,en.$domain,ipv6.$domain,nl.ipv6.$domain,en.ipv6.$domain"
+if [ "$ENABLE_BATCH" != True ]; then
+  subdomains="$subdomains,www.$domain,conn.$domain,en.conn.$domain,nl.conn.$domain,www.conn.$domain"
+fi
 if [ ! -z $REDIRECT_DOMAINS ];then
   subdomains="$subdomains,$REDIRECT_DOMAINS"
 fi
