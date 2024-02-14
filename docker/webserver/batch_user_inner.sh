25a934056d81bb697de5aae29cb53ecd314c1803
Sasha Romijn <github@mxsasha.eu>
2024-02-14 16:35:57 +0100

    Ref #1267 - Add htpasswd management scripts for batch API

---
+++ docker/webserver/batch_user_inner.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+HTPASSWD_FILE="/etc/nginx/htpasswd/external/batch_api.htpasswd"
+
+if [ ! -f "$HTPASSWD_FILE" ]; then
+  touch "$HTPASSWD_FILE"
+fi
+
+if [ "$1" = "add_update" ]; then
+  /usr/bin/htpasswd -B "$HTPASSWD_FILE" "$2"
+  /usr/sbin/nginx -s reload
+elif [ "$1" = "remove" ]; then
+  /usr/bin/htpasswd -D "$HTPASSWD_FILE" "$2"
+  /usr/sbin/nginx -s reload
+elif [ "$1" = "verify" ]; then
+  /usr/bin/htpasswd -v "$HTPASSWD_FILE" "$2"
+else
+  echo "Usage: $0 <add_update|remove|verify> <username>"
+  exit 1
+fi
