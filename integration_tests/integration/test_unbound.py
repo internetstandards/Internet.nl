22378c8747bf5036ad7d27f2575e0bd0c3f03f78
Johan Bloemberg <git@ijohan.nl>
2024-04-12 15:03:06 +0200

    [1.8.x] Resign connection test zone signatures every month.
    (cherry picked from commit 4826798890c8ccfa3cbd0fa176da66e77f9bae79)

---
+++ integration_tests/integration/test_unbound.py
@@ -0,0 +1,37 @@
+import time
+
+
+def test_unbound_ldns_signzone_cron(trigger_cron, docker_compose_exec):
+    """Ensure DNS zones are resigned regurarly."""
+
+    def get_rrsig_expiry():
+        return int(
+            docker_compose_exec(
+                "app", "dig x.a.conn.test-ns-signed.internet.test  +dnssec @unbound|grep 'RRSIG A'"
+            ).split()[8]
+        )
+
+    # get expiry time field from RRSIG record
+    expiry = get_rrsig_expiry()
+
+    time.sleep(1)
+
+    new_expiry = get_rrsig_expiry()
+
+    assert expiry == new_expiry, "sanity check"
+
+    # resign zones via cron script
+    print(trigger_cron("monthly/signzones", service="unbound"))
+
+    time.sleep(1)
+
+    # get expiry time field from RRSIG record
+    new_expiry = get_rrsig_expiry()
+
+    assert int(expiry) < int(new_expiry)
+
+
+def test_unbound_cron_running(docker_compose_exec):
+    """Ensure cron daemon is running"""
+
+    docker_compose_exec("unbound", "pgrep -f 'busybox crond'")
