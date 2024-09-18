b623d73d065ee470f1257a2fcecc23a601ac5faa
Johan Bloemberg <git@ijohan.nl>
2024-09-18 08:47:41 +0200

    Improve alerting
    - Add more alerting test domains
    - alert if at least 2 domains fail
    - don't test mail on domains that have no MX
    - also measure if probe passed or not
    - don't measure scores for probes that don't count towards the total score (appsecpriv)
    - add metrics for failed probes on timeout

--- docker/cron/periodic/15min/tests.py
+++ docker/cron/periodic/15min/tests.py
@@ -21,7 +21,9 @@ DEBUG = "--debug" in sys.argv
 # file to write metrics to https://github.com/prometheus/node_exporter?tab=readme-ov-file#textfile-collector
 OUTPUT_TEXTFILE = "/prometheus-textfile-directory/tests.prom"
 
-TEST_TIMEOUT = int(os.environ.get("INTERNETNL_CACHE_TTL"))
+
+DEFAULT_TEST_TIMEOUT = 200
+TEST_TIMEOUT = int(os.environ.get("INTERNETNL_CACHE_TTL", DEFAULT_TEST_TIMEOUT))
 REQUEST_TIMEOUT = 30
 
 TESTS = ["site", "mail"]
@@ -33,12 +35,32 @@ INTERNETNL_DOMAINNAME = os.environ.get("INTERNETNL_DOMAINNAME")
 URL_BASE = f"http://{IPV4_IP_APP_INTERNAL}:8080"
 HEADERS = {"Host": INTERNETNL_DOMAINNAME}
 
-# domain's to use in website tests
-WEBSITE_TEST_DOMAINS = [
-    "example.nl",
-    "example.com",
-]
-
+TEST_DOMAINS = {
+    # domain's to use in website tests
+    "site": [
+        "internet.nl",
+        "example.nl",
+        "example.com",
+        "internetsociety.org",
+        "ripe.net",
+        "surf.nl",
+        "ecp.nl",
+        "forumstandaardisatie.nl",
+        "minez.nl",
+    ],
+    # domain's to use in mail tests
+    "mail": [
+        "internetsociety.org",
+        "ripe.net",
+        "surf.nl",
+        "ecp.nl",
+        # these are currently really slow and will probably improve when
+        # we switch to sslyze, for now disable these in monitoring
+        # "internet.nl",
+        # "forumstandaardisatie.nl",
+        # "minez.nl",
+    ],
+}
 
 METRIC_PROBE_DONE = Gauge("tests_probe_done_total", "Whether the probe completed.", ["test", "domain", "probe"])
 METRIC_PROBE_SUCCESS = Gauge("tests_probe_success_total", "Whether the probe succeeded.", ["test", "domain", "probe"])
@@ -46,16 +68,20 @@ METRIC_PROBE_RUNTIME = Gauge(
     "tests_probe_runtime_seconds", "Amount of time probe ran before done.", ["test", "domain", "probe"]
 )
 METRIC_PROBE_SCORE = Gauge("tests_probe_score", "Score of the probe.", ["test", "domain", "probe"])
+METRIC_PROBE_PASSED = Gauge("tests_probe_pass", "Probe has passed.", ["test", "domain", "probe"])
 
 METRIC_TEST_RUN = Gauge("tests_test_run_total", "Test that have been run.", ["test", "domain"])
 METRIC_TEST_CACHE = Gauge("tests_test_cached_total", "Test runs that returned cached results.", ["test", "domain"])
 METRIC_TEST_FAILURE = Gauge("tests_test_failure_total", "Test runs that failed.", ["test", "domain"])
 METRIC_TEST_SUCCESS = Gauge("tests_test_success_total", "Test runs that succeeded.", ["test", "domain"])
-METRIC_TEST_TIMEOUT = Gauge("tests_test_timeout", "Test that ran into timeout.", ["test", "domain"])
+METRIC_TEST_TIMEOUT = Gauge("tests_test_timeout_total", "Test that ran into timeout.", ["test", "domain"])
 METRIC_TEST_RUNTIME = Gauge("tests_test_runtime_seconds", "Amount of time test ran before done.", ["test", "domain"])
+METRIC_TEST_SCORE = Gauge("tests_test_score", "Total score of all probes in the test.", ["test", "domain"])
 
 
 def run_tests_on_domain(test, domain):
+    test_start = int(time.time())
+
     # initiate the test
     r = requests.get(
         f"{URL_BASE}/{test}/probes/{domain}/?{time.time()}",
@@ -73,7 +99,6 @@ def run_tests_on_domain(test, domain):
         return
 
     # poll probes until done
-    test_start = int(time.time())
     finished_probes = set()
     while int(time.time()) < test_start + TEST_TIMEOUT:
         # get probe status
@@ -99,15 +124,26 @@ def run_tests_on_domain(test, domain):
 
         # stop when all probes are finished
         if not [p for p in probes if not p["done"]]:
+            METRIC_TEST_SUCCESS.labels(test, domain).set(1)
             break
 
         time.sleep(1)
     else:
+        # test timed out because one or more of the probes was not done within time
         METRIC_TEST_TIMEOUT.labels(test, domain).set(1)
+        for probe in probes:
+            if probe["name"] in finished_probes:
+                continue
+            # record not finished probes as failed
+            METRIC_PROBE_DONE.labels(test, domain, probe["name"]).set(probe["done"])
+            METRIC_PROBE_RUNTIME.labels(test, domain, probe["name"]).set(int(time.time() - test_start))
+            if probe["done"]:
+                METRIC_PROBE_SUCCESS.labels(test, domain, probe["name"]).set(probe["success"])
 
     METRIC_TEST_RUNTIME.labels(test, domain).set(int(time.time() - test_start))
 
     # get additional metrics like score
+    scores = list()
     for probe_name in finished_probes:
         try:
             r = requests.get(
@@ -119,18 +155,29 @@ def run_tests_on_domain(test, domain):
             r.raise_for_status()
             if r.status_code == 200:
                 probe_result = r.json()
-                METRIC_PROBE_SCORE.labels(test, domain, probe_name).set(probe_result["totalscore"])
+                # only measure probe scores that count towards total score
+                if probe_result["maxscore"]:
+                    METRIC_PROBE_SCORE.labels(test, domain, probe_name).set(probe_result["totalscore"])
+                    scores.append(probe_result["totalscore"])
+                METRIC_PROBE_PASSED.labels(test, domain, probe_name).set(probe_result["verdict"] == "passed")
         except Exception:
             log.exception("failed to get probe score")
+
+    if scores:
+        METRIC_TEST_SCORE.labels(test, domain).set(max(min(int(sum(scores) / len(scores)), 100), 0))
+    else:
+        METRIC_TEST_SCORE.labels(test, domain).set(0)
 
 
 def run_tests():
     for test in TESTS:
-        for domain in WEBSITE_TEST_DOMAINS:
+        for domain in TEST_DOMAINS[test]:
             log.info(f"testing: {test} {domain}")
             METRIC_TEST_RUN.labels(test, domain).set(1)
             METRIC_TEST_CACHE.labels(test, domain).set(0)
             METRIC_TEST_FAILURE.labels(test, domain).set(0)
+            METRIC_TEST_TIMEOUT.labels(test, domain).set(0)
+            METRIC_TEST_SUCCESS.labels(test, domain).set(0)
             try:
                 run_tests_on_domain(test, domain)
             except Exception:
