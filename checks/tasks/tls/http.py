a1722b4185c802c3c589a4a9daf3ddad25f1dd30
Sasha Romijn <github@mxsasha.eu>
2024-10-18 16:56:38 +0200

    tweaks

--- checks/tasks/tls/http.py
+++ checks/tasks/tls/http.py
@@ -15,19 +15,17 @@ from checks.tasks.http_headers import (
 def http_checks(af_ip_pair, url, task):
     """
     Perform the HTTP header and HTTPS redirection checks for this webserver.
-
     """
     forced_https_score, forced_https = forced_http_check(af_ip_pair, url, task)
     header_checkers = [
         HeaderCheckerContentEncoding(),
         HeaderCheckerStrictTransportSecurity(),
     ]
-    header_results = http_headers_check(af_ip_pair, url, header_checkers, task)
-
     results = {
         "forced_https": forced_https,
         "forced_https_score": forced_https_score,
     }
+    header_results = http_headers_check(af_ip_pair, url, header_checkers, task)
     results.update(header_results)
     return results
 
