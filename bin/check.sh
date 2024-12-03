abedff94187ac081448a54e84d70ec324a491f99
Johan Bloemberg <git@ijohan.nl>
2024-12-03 23:24:26 +0100

    Add makemigrations command

--- bin/check.sh
+++ bin/check.sh
@@ -7,7 +7,7 @@ fail=0
 requirements_files="requirements.in requirements-dev.in"
 echo $requirements_files | xargs -n1 pip-compile --quiet
 if [ ! -z "$(git status --porcelain $requirements_files)" ];then
-  echo "Requirements .in files have not all been compiled into .txt files and commited to Git!"
+  echo -e "\e[31mRequirements .in files have not all been compiled into .txt files and commited to Git!"
   git status --porcelain $requirements_files
   fail=1
 fi
@@ -21,7 +21,7 @@ if [ $exit_code -ne 1 ];then
   echo "exit code: $?"
   echo "output: $output"
   echo
-  echo "Webserver should fail with exit code 1 if authentication/allowlist is not set when DEBUG=True"
+  echo -e "\e[31mWebserver should fail with exit code 1 if authentication/allowlist is not set when DEBUG=True"
   fail=1
 fi
 
