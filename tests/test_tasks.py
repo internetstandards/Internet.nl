dc37d0b40063d318cd9a42edcf17361cc9ba82b3
Sasha Romijn <github@mxsasha.eu>
2025-01-29 15:00:55 +0100

    Remove obsolete test - unbound was removed in #1578

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -6,7 +6,6 @@
 from internetnl import log
 from internetnl.celery import dummy_task
 import time
-from checks.tasks.ipv6 import web
 
 
 def wait_for_result(task_id):
@@ -34,41 +33,3 @@ def test_various_workers(custom_celery_worker):
     task_id = dummy_task.si(2).apply_async()
     result = wait_for_result(task_id)
     assert result == 4
-
-
-# @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS", "") == "True", reason="Redis hang? at github actions")
-# @pytest.mark.skip
-def test_task_with_unbound_context(custom_celery_worker):
-    """Verify a task can be started with unbound context. The unbound context works fine in prefork,
-    but does it also function well in gevent and eventlet situations. We'll find out.
-
-    Note: this will not lead to more test coverage as there the execution of this code happens somewhere else.
-    """
-    # Todo: test if there is a connection with a fixture.
-    log.info("Creating a task")
-    task = web.si("internet.nl").apply_async()
-    log.info("Waiting for result")
-    result = wait_for_result(task)
-    log.info("Done waiting for result")
-
-    """
-    This is a live test, so it needs an internet connection.
-    The score will vary, but there will be a result with the domain name in there, regardless of the measurement.
-    (
-         'web',
-         {'domains': [{'domain': 'internet.nl',
-                       'score': 0,
-                       'v4_bad': [],
-                       'v4_good': [],
-                       'v6_bad': [],
-                       'v6_conn_diff': [],
-                       'v6_good': []}],
-          'score': 0,
-          'simhash_distance': 110,
-          'simhash_score': 0},
-        )
-
-    """
-
-    _, measurement = result
-    assert measurement["domains"][0]["domain"] == "internet.nl"
