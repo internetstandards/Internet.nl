716d04ad38d65e42b269752c41bcb78212c32705
Johan Bloemberg <git@ijohan.nl>
2025-03-13 18:44:48 +0100

    Suppress warning during makemigration check

--- bin/lint.sh
+++ bin/lint.sh
@@ -10,7 +10,7 @@ pylama --skip "**/migrations/*" ${@} || fail=1
 black --line-length 120 --check ${@} || fail=1
 shellcheck -e SC1071 docker/cron/periodic/*/* || fail=1
 
-if ! SKIP_SECRET_KEY_CHECK=True CACHE_LOCATION= ENABLE_BATCH= ./manage.py makemigrations --noinput --check --dry-run; then
+if ! SKIP_SECRET_KEY_CHECK=True CACHE_LOCATION= ENABLE_BATCH= DJANGO_DATABASE=dev ./manage.py makemigrations --noinput --check --dry-run; then
     echo -e "\e[31mNot all migrations have been created after changes to \`models.py\`. Run \`make makemigrations\` to update migrations.\e[0m"
     fail=1
 fi
