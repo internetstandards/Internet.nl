9d704e942657add11f32afc769a8b73bd9de0b6e
Khdulkadir <144004145+Khdulkadir@users.noreply.github.com>
2025-04-03 18:36:08 +0200

    update added article load more button and more refactoring

---
+++ frontend/js/load-more.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll(".card:nth-child(n+6)");
+const loadMoreBtn = document.getElementById("load-more");
+let visibleCount = 0;
+const batchSize = 3;
+
+loadMoreBtn.addEventListener("click", function () {
+  for (let i = visibleCount; i < visibleCount + batchSize; i++) {
+    if (cards[i]) {
+      cards[i].classList.add('visible');
+    }
+  }
+  visibleCount += batchSize;
+
+  if (visibleCount >= cards.length) {
+    loadMoreBtn.classList.add('hidden');
+  }
+});
