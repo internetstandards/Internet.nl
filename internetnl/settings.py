2b228ca65f2483adae136f4853774d1c03b287a0
Johan Bloemberg <git@ijohan.nl>
2026-06-30 16:20:11 +0200

    Revert "fix issues with gevent based workers"
    This reverts commit a437d6ad1284e24f6004ea961a2df2c52a39debf.

--- internetnl/settings.py
+++ internetnl/settings.py
@@ -291,9 +291,8 @@ CELERY_BROKER_HEARTBEAT = 0  # Workaround for https://github.com/celery/celery/i
 CELERY_TASK_ACKS_LATE = True
 CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 
-# prevent issues with gevent based workers
-# https://github.com/internetstandards/Internet.nl/pull/2102
-CELERY_RESULT_BACKEND_THREAD_SAFE = False
+# fix redis connection leaks
+CELERY_RESULT_BACKEND_THREAD_SAFE = True
 
 # used for celery-exporter
 CELERY_WORKER_SEND_TASK_EVENTS = True
